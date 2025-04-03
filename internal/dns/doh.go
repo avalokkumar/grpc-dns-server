5418026e828ebb0c2775c92e2a84488273feef00
Alok Vishwakarma <avalokkumar@gmail.com>
2025-04-03 09:31:41 +0530

    Delete internal directory

--- internal/dns/doh.go
+++
@@ -1,9 +0,0 @@
-package dns
-
-import "errors"
-
-// ResolveDoH is a placeholder function for DNS-over-HTTPS (DoH) resolution
-func ResolveDoH(domain string) (string, error) {
-	// Return an error indicating that DoH resolution is not implemented
-	return "", errors.New("DoH resolution not implemented")
-}
